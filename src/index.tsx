b793752931343fc70eeff07cb888fa86d8f75214
Denis <ITUlvis@gmail.com>
2023-07-12 00:14:29 +0300

    add React functional component

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-const content = (
-    <div>
-        <ul>
-            <li>List item 1</li>
-            <li>List item 2</li>
-        </ul>
-    </div>
-)
+function App() {
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi
+                culpa minus reprehenderit non. Doloribus, molestiae nam? Ab
+                quaerat nam aut quis placeat quibusdam aperiam, minus asperiores
+                natus officia doloremque sed!
+            </p>
+        </div>
+    )
+}
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode>{content}</React.StrictMode>)
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
