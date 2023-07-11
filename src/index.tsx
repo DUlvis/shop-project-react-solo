edcc2dd3e2045577674617b82ab3f78946f405c2
Denis <ITUlvis@gmail.com>
2023-07-12 00:25:23 +0300

    add React Fragments

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-function App() {
+const Title = () => {
+    return <h1>Hello World</h1>
+}
+const Content = () => {
     return (
-        <div>
-            <h1>Hello World</h1>
+        <React.Fragment>
             <p>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi
                 culpa minus reprehenderit non. Doloribus, molestiae nam? Ab
                 quaerat nam aut quis placeat quibusdam aperiam, minus asperiores
                 natus officia doloremque sed!
             </p>
-        </div>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi
+                culpa minus reprehenderit non. Doloribus, molestiae nam? Ab
+                quaerat nam aut quis placeat quibusdam aperiam, minus asperiores
+                natus officia doloremque sed!
+            </p>
+        </React.Fragment>
+    )
+}
+const App = () => {
+    return (
+        <>
+            <Title />
+            <Content />
+        </>
     )
 }
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
