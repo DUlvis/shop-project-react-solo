37b9746ca544ac7d3e2275d09373bf5788c7f6c8
Denis <ITUlvis@gmail.com>
2023-07-20 14:25:29 +0300

    add StyledEngineProvider to override MUI styles

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,12 +1,15 @@
 import CssBaseline from '@mui/material/CssBaseline'
 import Header from 'container/Header/Header'
+import { StyledEngineProvider } from '@mui/material/styles'
 
 type Props = {}
 const App = (props: Props) => {
     return (
         <>
-            <CssBaseline />
-            <Header />
+            <StyledEngineProvider injectFirst>
+                <CssBaseline />
+                <Header />
+            </StyledEngineProvider>
         </>
     )
 }
